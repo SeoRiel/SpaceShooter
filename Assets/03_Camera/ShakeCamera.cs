efa498a17fd90b1ae767c15be5e1e4573358042b
Gwang Chun, Seo <35924301+SeoRiel@users.noreply.github.com>
2021-08-24 00:30:35 +0900

    카메라 추적 기능 수정
    카메라 추적 기능 수정

--- Assets/03_Camera/ShakeCamera.cs
+++ Assets/03_Camera/ShakeCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+public class ShakeCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     private void Start()
