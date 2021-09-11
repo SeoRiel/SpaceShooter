1feeab279e708af656b9d7420fd110491179bca7
Gwang Chun, Seo <35924301+SeoRiel@users.noreply.github.com>
2021-09-11 10:20:31 +0900

    씬 분리 및 책 최종 마무리
    LoadSceneManager를 이용한 씬 분리, 병합 기능 추가

--- Assets/10_UI/Textures/Scripts/UIManager.cs
+++ Assets/10_UI/Textures/Scripts/UIManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public void OnClickStartButton()
     {
-        Debug.Log("Click Button");
+        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Play", LoadSceneMode.Additive);
     }
 }
